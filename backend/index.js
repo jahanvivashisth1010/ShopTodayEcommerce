cee185399a52efd3268cafa448a2d8971dd6326c
Jahanvi Sharma <jahanvivashisth1010@gmail.com>
2026-07-09 09:03:49 -0700

    Add The code which helps in deploy bpth build at same  time

--- backend/index.js
+++ backend/index.js
@@ -40,6 +40,19 @@ app.use("/api/orders", require("./routes/orderRoutes"));
 app.use("/api/payment", require("./routes/paymentRoutes"));
 app.use("/api/analytics", require("./routes/analyticsRoutes"));
 
+// Serve frontend in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.use((req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("ShopNest API is running in Development mode...");
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
